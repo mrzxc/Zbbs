cd58f3ba567d35da4e27a73a11694ace54afdd0a
mrzxc <1055997094@qq.com>
2017-06-26 00:45:58 +0800

    login

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 var express = require('express');
+
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.set('port', process.env.PORT || 3000);
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+app.set('view engine', 'jade');
 
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+app.set('views', './views/pages')
+
+app.use(express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.render('index', {
+    title: '主页'
+  })
+})
+
+app.get('/login', function(req, res) {
+  res.render('login', {
+    title: 'login'
+  })
+})
+
+app.use(function(req, res, next) {
+  res.status(404);
+  res.render('404');
+})
+
+app.use(function(req, res, next) {
+  console.error(err.stack)
+  res.status(500);
+  res.render('500');
+})
+
+app.listen(app.get('port'), function() {
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminaate');
+})
---
+++ models/customers.js
@@ -0,0 +1,6 @@
+var mongoose = require('mongoose');
+var Orders = require('./orders.js');
+
+var customerSchema = mongoose.Schema({
+  
+})
